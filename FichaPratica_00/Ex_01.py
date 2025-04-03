36e874834a33eb5e0796fb378922d4f18e8dd094
aws12 <aws12@CESAEPRT.WAN>
2025-04-03 17:25:21 +0100

    ficha pratica 00

---
+++ FichaPratica_00/Ex_01.py
@@ -0,0 +1,8 @@
+#pedir ao user dois numeros inteiros
+numero1 = int(input("digite o primeiro número:"))
+numero2 = int (input("digite o segundo número:"))
+
+#guardar o resultado numa variavel | fazer soma
+resultadoSoma = numero1 + numero2
+#print do resultado
+print ("o resultado da soma é =", resultadoSoma)
