83b5261c9f7d6c6f1399754f5331e35ec8352a13
joao <joaofmp99@gmail.com>
2025-04-10 14:49:29 +0100

    finish ex15 start 16

---
+++ Intro Programação/fichaPratica_01/Fp01_Ex16.py
@@ -0,0 +1,24 @@
+#pedir ao user para inserir o valor a 
+valor=int(input"Por favor inserir valor")
+
+#calcular quantidade de notas de cada denominação
+
+notas200 = valor/200
+resto200 = valor%200
+
+notas100= valor/100
+resto100 = valor%100
+
+notas50 = valor/50
+resto50 = valor%50
+
+notas20 = valor/20
+resto20 = valor%20
+
+notas10 = valor/10
+resto10 = valor%10
+
+notas5 = valor/5
+resto5 = valor%5
+
+
